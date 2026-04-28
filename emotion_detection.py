f44d9bf0af9a1381f9e23018a9ed0b897c4df6da
Jackie Recendez <jackierecendez@gmail.com>
2026-04-28 14:46:04 -0400

    Second commit should just create a new file emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+# here write the function to run emotion detection 
+#using the appropriatemotion Detection function. name the function emotion_detector
+
+def emotion_detector(text_to_analyze):
+    #assume text to be analysed is passed to the function
+    #as an argument and stored in the variable text to analyze
+    return text
+    
